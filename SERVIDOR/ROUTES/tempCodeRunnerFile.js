c9c14b01f77c6ae492787646895a59d9b4fba28d
Stefanny19 <stefannymv1902@gmail.com>
2024-04-05 07:34:28 -0500

    cambios en server

--- SERVIDOR/ROUTES/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-app.post('/producto/añadir', (req, res) => {
-    
-    const {nombre_usuario, apellido_usuario, correo, tipo_documento, contraseña, telefono, idRol } = req.body;
-
-    // Llamar al controlador para agregar el producto al carrito con la cantidad especificada
-    controladorServer.s_añadirUsuario(nombre_usuario, apellido_usuario, correo, tipo_documento, contraseña, telefono, idRol);
-
-    res.send('usuario añadido');
-});
