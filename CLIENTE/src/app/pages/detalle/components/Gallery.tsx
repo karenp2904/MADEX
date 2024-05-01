5990ac34dda3fa31a09640d698bc99a3eb63823a
karenp2904 <karen2005yulieth@gmail.com>
2024-04-30 21:45:34 -0500

    detalle ready

--- CLIENTE/src/app/pages/detalle/components/Gallery.tsx
+++ CLIENTE/src/app/pages/detalle/components/Gallery.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 const Gallery = ({ productName }) => {
-    const [images, setImages] = useState([]);
+    const [image, setImage] = useState(1);
 
     useEffect(() => {
         const fetchImages = async () => {
@@ -9,7 +9,7 @@ const Gallery = ({ productName }) => {
                 const response = await fetch(`http://localhost:3000/producto/CatalogoImagenes/${productName}`);
                 if (response.ok) {
                     const data = await response.json();
-                    setImages(data.imagenes); // Suponiendo que la respuesta del servidor contiene un arreglo de imágenes
+                    //setImages(data.imagenes); // Suponiendo que la respuesta del servidor contiene un arreglo de imágenes
                 } else {
                     console.error('Error al obtener las imágenes del producto');
                 }
@@ -29,12 +29,27 @@ const Gallery = ({ productName }) => {
                 <div className="md:flex-1 px-4">
                     <div>
                         <div className="h-64 md:h-80 rounded-lg bg-gray-100 mb-4">
-                            {images.map((image, index) => (
+                            {[1, 2, 3, 4].map((i) => (
                                 <div
-                                    key={index}
-                                    className={`h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center`}
+                                    key={i}
+                                    className={`h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center ${image === i ? "" : "hidden"
+                                        }`}
                                 >
-                                    <img src={image} alt={`Image ${index}`} className="max-h-full max-w-full" />
+                                    <span className="text-5xl">{i}</span>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex -mx-2 mb-4">
+                            {[1, 2, 3, 4].map((i) => (
+                                <div key={i} className="flex-1 px-2">
+                                    <button
+                                        className={`focus:outline-none w-full rounded-lg h-15 md:h-20 bg-gray-100 flex items-center justify-center ${image === i ? "ring-2 ring-indigo-300 ring-inset" : ""
+                                            }`}
+                                        onClick={() => setImage(i)}
+                                    >
+                                        <span className="text-2xl">{i}</span>
+                                    </button>
                                 </div>
                             ))}
                         </div>
